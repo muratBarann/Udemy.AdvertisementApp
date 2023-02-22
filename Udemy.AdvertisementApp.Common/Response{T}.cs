c10b28cfbf40ca94362dd973632d886b1323f473
Murat Baran <mrtbrn911@gmail.com>
2023-02-22 16:55:15 +0300

    Common katmanı response yapısı oluşturuldu

---
+++ Udemy.AdvertisementApp.Common/Response{T}.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.AdvertisementApp.Common
+{
+    //BU response turu data tasiyan turudur.
+    public class Response<T> : Response , IResponse<T>
+    {
+
+        //Burada responseType ve message'ı datasız bir sekilde base class'a gonderdik
+        public Response(ResponseType responseType,string message) : base(responseType,message) 
+        {
+        }
+
+        public Response(ResponseType responseType, T data): base(responseType)
+        {
+            Data = data;
+        }
+
+        public Response(T data, List<CustomValidationError> errors) : base(ResponseType.ValidationError)
+        {
+            Data=data;
+            ValidationError = errors;
+        }
+
+        public T Data { get; set; }
+        public List<CustomValidationError> ValidationError{ get; set; }
+    }
+}
